b31b056c397840762c4882bf499f2b00db1887e4
Frog-kt <kasuo.takumi@gmail.com>
2021-06-15 07:33:40 +0900

    Update App.svelte

--- idp-front/src/App.svelte
+++ idp-front/src/App.svelte
@@ -1,8 +1,26 @@
 <script>
+	let promise;
+	const handler = async () => {
+		const uri = "http://localhost:3001/redirect";
+		await fetch(uri, {
+			method: "POST",
+			// credentials: "include",
+			// headers: { "Content-Type": "application/json" },
+		})
+			.then((result) => {
+				console.log("response");
+				console.log(result);
+			})
+			.catch((err) => {
+				console.log("error");
+				console.log(err);
+			});
+	};
 </script>
 
 <main>
-	<h1>hello!</h1>
+	<h1>Redirect test</h1>
+	<button on:click={handler}>別ドメインへのリダイレクト</button>
 </main>
 
 <style>
