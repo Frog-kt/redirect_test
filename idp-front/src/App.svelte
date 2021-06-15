3f8a9002ec9ab2ea30931edc356035873c0936bd
かえる <kasuo.takumi@gmail.com>
2021-06-15 10:20:53 +0900

    Update App.svelte

--- idp-front/src/App.svelte
+++ idp-front/src/App.svelte
@@ -1,27 +1,27 @@
 <script>
-	let promise;
-	const handler = async () => {
-		const uri = "http://localhost:3000/redirect";
-		await fetch(uri, {
-			method: "POST",
-			origin: "http://localhost/",
-			// credentials: "include",
-			// headers: { "Content-Type": "application/json" },
-		})
-			.then((result) => {
-				console.log("response");
-				console.log(result);
-			})
-			.catch((err) => {
-				console.log("error");
-				console.log(err);
-			});
-	};
+  let promise;
+  const handler = async () => {
+    const uri = "http://localhost:3000/redirect";
+    await fetch(uri, {
+      method: "POST",
+    //   redirect: "manual",
+      // credentials: "include",
+      // headers: { "Content-Type": "application/json" },
+    })
+      .then((result) => {
+        console.log("response");
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err);
+      });
+  };
 </script>
 
 <main>
-	<h1>Redirect test</h1>
-	<button on:click={handler}>別ドメインへのリダイレクト</button>
+  <h1>Redirect test</h1>
+  <button on:click={handler}>別ドメインへのリダイレクト</button>
 </main>
 
 <style>
