98a417051375e48e5194e59a4edaa732f4c7b83b
Frog-kt <kasuo.takumi@gmail.com>
2021-06-18 22:54:27 +0900

    Update App.svelte

--- idp-front/src/App.svelte
+++ idp-front/src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-  let promise;
-  const handler = async () => {
-    const uri = "http://localhost:3000/redirect";
-    await fetch(uri, {
-      method: "POST",
-      // redirect: "manual",
-      // credentials: "include",
-      // headers: { "Content-Type": "application/json" },
-    })
-      .then((result) => {
-        console.log("response");
-        // console.log(result);
-        if (result.redirected) {
-          window.location.href = result.url;
-        }
-      })
-      .catch((err) => {
-        console.log("error");
-        console.log(err);
-      });
-  };
+	let promise;
+	const handler = async () => {
+		const uri = "http://localhost:3000/redirect";
+		await fetch(uri, {
+			method: "POST",
+			redirect: "manual",
+			// credentials: "include",
+			// headers: { "Content-Type": "application/json" },
+		})
+			.then((result) => {
+				console.log("response");
+				console.log(result);
+				window.location.href = result.url;
+			})
+			.catch((err) => {
+				console.log("error");
+				console.log(err);
+			});
+	};
 </script>
 
 <main>
-  <h1>Redirect test</h1>
-  <button on:click={handler}>別ドメインへのリダイレクト</button>
+	<h1>Redirect test</h1>
+	<button on:click={handler}>別ドメインへのリダイレクト</button>
 </main>
 
 <style>
